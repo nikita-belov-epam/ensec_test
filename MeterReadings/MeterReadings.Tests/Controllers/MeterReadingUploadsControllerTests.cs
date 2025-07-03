7cca7a9ea526fdabfa7afb52c9feab6ed0657f54
nikita_belov <nikita_belov@epam.com>
2025-07-03 19:28:55 +0400

    More tests

--- MeterReadings/MeterReadings.Tests/Controllers/MeterReadingUploadsControllerTests.cs
+++ MeterReadings/MeterReadings.Tests/Controllers/MeterReadingUploadsControllerTests.cs
@@ -5,14 +5,11 @@ using MeterReadings.Entities.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace MeterReadings.Tests.Controllers
 {
+    [TestFixture]
     public class MeterReadingUploadsControllerTests
     {
         private ApplicationDbContext GetDbContext()
@@ -44,7 +41,6 @@ namespace MeterReadings.Tests.Controllers
 
             var okResult = result as OkObjectResult;
             MeterReadingUploadResultDto? resultDto = okResult?.Value as MeterReadingUploadResultDto;
-
             Assert.Multiple(() =>
             {
                 Assert.That(okResult, Is.Not.Null, "Result should be OkObjectResult");
@@ -53,6 +49,131 @@ namespace MeterReadings.Tests.Controllers
                 Assert.That(resultDto?.Failed, Is.EqualTo(0), "Failed count should be 0");
                 Assert.That(context.MeterReadings.Count(), Is.EqualTo(1), "Should have 1 meter reading in DB");
             });
+        }
+
+        [Test]
+        public async Task UploadMeterReadings_InvalidAccountId_Fails()
+        {
+            var context = GetDbContext();
+            var controller = new MeterReadingUploadsController(context);
+            var csv = "AccountId,MeterReadingDateTime,MeterReadValue\n9999,22/04/2019 09:24,12345";
+            var file = CreateTestFile(csv);
+
+            var result = await controller.UploadMeterReadings(file);
+
+            var okResult = result as OkObjectResult;
+            var resultDto = okResult?.Value as MeterReadingUploadResultDto;
+            Assert.Multiple(() =>
+            {
+                Assert.That(resultDto, Is.Not.Null);
+                Assert.That(resultDto?.Success, Is.EqualTo(0));
+                Assert.That(resultDto?.Failed, Is.EqualTo(1));
+                Assert.That(context.MeterReadings.Count(), Is.EqualTo(0));
+            });
+        }
+
+        [Test]
+        public async Task UploadMeterReadings_InvalidDate_Fails()
+        {
+            var context = GetDbContext();
+            var controller = new MeterReadingUploadsController(context);
+            var csv = "AccountId,MeterReadingDateTime,MeterReadValue\n1234,not-a-date,12345";
+            var file = CreateTestFile(csv);
+
+            var result = await controller.UploadMeterReadings(file);
+
+            var okResult = result as OkObjectResult;
+            var resultDto = okResult?.Value as MeterReadingUploadResultDto;
+            Assert.Multiple(() =>
+            {
+                Assert.That(resultDto, Is.Not.Null);
+                Assert.That(resultDto?.Success, Is.EqualTo(0));
+                Assert.That(resultDto?.Failed, Is.EqualTo(1));
+                Assert.That(context.MeterReadings.Count(), Is.EqualTo(0));
+            });
+        }
+
+        [Test]
+        public async Task UploadMeterReadings_InvalidMeterValue_Fails()
+        {
+            var context = GetDbContext();
+            var controller = new MeterReadingUploadsController(context);
+            var csv = "AccountId,MeterReadingDateTime,MeterReadValue\n1234,22/04/2019 09:24,12";
+            var file = CreateTestFile(csv);
+
+            var result = await controller.UploadMeterReadings(file);
+
+            var okResult = result as OkObjectResult;
+            var resultDto = okResult?.Value as MeterReadingUploadResultDto;
+            Assert.Multiple(() =>
+            {
+                Assert.That(resultDto, Is.Not.Null);
+                Assert.That(resultDto?.Success, Is.EqualTo(0));
+                Assert.That(resultDto?.Failed, Is.EqualTo(1));
+                Assert.That(context.MeterReadings.Count(), Is.EqualTo(0));
+            });
+        }
+
+        [Test]
+        public async Task UploadMeterReadings_DuplicateInFile_OneSuccessOneFail()
+        {
+            var context = GetDbContext();
+            var controller = new MeterReadingUploadsController(context);
+            var csv = "AccountId,MeterReadingDateTime,MeterReadValue\n1234,22/04/2019 09:24,12345\n1234,22/04/2019 09:24,12345";
+            var file = CreateTestFile(csv);
+
+            var result = await controller.UploadMeterReadings(file);
+
+            var okResult = result as OkObjectResult;
+            var resultDto = okResult?.Value as MeterReadingUploadResultDto;
+            Assert.Multiple(() =>
+            {
+                Assert.That(resultDto, Is.Not.Null);
+                Assert.That(resultDto?.Success, Is.EqualTo(1));
+                Assert.That(resultDto?.Failed, Is.EqualTo(1));
+                Assert.That(context.MeterReadings.Count(), Is.EqualTo(1));
+            });
+        }
+
+        [Test]
+        public async Task UploadMeterReadings_DuplicateInDatabase_Fails()
+        {
+            var context = GetDbContext();
+            context.MeterReadings.Add(new MeterReading
+            {
+                AccountId = 1234,
+                MeterReadingDateTime = new DateTime(2019, 4, 22, 9, 24, 0),
+                MeterReadValue = 12345,
+                CreatedAt = DateTime.UtcNow
+            });
+            context.SaveChanges();
+            var controller = new MeterReadingUploadsController(context);
+            var csv = "AccountId,MeterReadingDateTime,MeterReadValue\n1234,22/04/2019 09:24,12345";
+            var file = CreateTestFile(csv);
+
+            var result = await controller.UploadMeterReadings(file);
+
+            var okResult = result as OkObjectResult;
+            var resultDto = okResult?.Value as MeterReadingUploadResultDto;
+            Assert.Multiple(() =>
+            {
+                Assert.That(resultDto, Is.Not.Null);
+                Assert.That(resultDto?.Success, Is.EqualTo(0));
+                Assert.That(resultDto?.Failed, Is.EqualTo(1));
+                Assert.That(context.MeterReadings.Count(), Is.EqualTo(1));
+            });
+        }
+
+        [Test]
+        public async Task UploadMeterReadings_EmptyFile_ReturnsBadRequest()
+        {
+            var context = GetDbContext();
+            var controller = new MeterReadingUploadsController(context);
+            var file = CreateTestFile(string.Empty);
+
+            var result = await controller.UploadMeterReadings(file);
+
+            Assert.That(result, Is.TypeOf<BadRequestObjectResult>());
         }
     }
 }
